17acba66624ed98f748684cd06a967b2d474da28
Lili Vecino <lili@MacBook-Pro-de-Apple.local>
2025-05-07 13:00:42 -0600

    First commit

---
+++ src/main/java/com/tecdesoftware/market/MarketApp3cApplication.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApp3cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApp3cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecdesoftware/market/MarketApp3cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApp3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
